24929059ff8c1265a68ee3e85eaec14725d6c2b3
Alexa Iordache <alexaiordache@Alexas-MacBook-Pro.local>
2023-04-19 21:41:38 +0300

    trying to add image upload

--- frontend/src/app/main-page/main-page.component.ts
+++ frontend/src/app/main-page/main-page.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,4 +9,26 @@ import { Component } from '@angular/core';
 export class MainPageComponent {
 
   panelOpenState = false;
+
+  // fileName = '';
+
+  // constructor(private http: HttpClient) {}
+
+  // onFileSelected(event: any) {
+
+  //     const file:File = event.target.files[0];
+
+  //     if (file) {
+
+  //         this.fileName = file.name;
+
+  //         const formData = new FormData();
+
+  //         formData.append("thumbnail", file);
+
+  //         const upload$ = this.http.post("/api/thumbnail-upload", formData);
+
+  //         upload$.subscribe();
+  //     }
+  // }
 }
